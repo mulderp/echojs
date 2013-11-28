d8973fafa511c2cc798eef0e7ee8025c65a7d289
Patrick Mulder <mulder.patrick@gmail.com>
2013-11-28 21:53:26 +0100

    prepare sha password lookup

--- api/users.js
+++ api/users.js
@@ -17,8 +17,6 @@ module.exports = {
               'username', raw.username,
               'password', raw.password
           );
-        }).then(function() {
-          return that.lookupUserByUsername(raw.username);
         }).then(function(){
             return client.hmgetAsync('user:'+userId, 'username', 'email');
         });
@@ -26,24 +24,51 @@ module.exports = {
 
   checkLogin: function(raw) {
     var that = this;
-    return this.lookupUserByUsername(raw.username)
+    return this.lookupUserID(raw.username)
       .then(function(id) {
-        if (id === null) { throw new Error("no user") };
-        return that.lookupUserByID(id);
+        if (id === null) { throw new Error("No match for the specified username / password pair."
+) };
+        return that.checkUserCredentials(id, raw.password);
       })
   },
 
-  lookupUserByID: function(id) {
-    return client.hgetallAsync("user:" + id)
-      .then(function(data) {
-        console.log(data);
-//     hp = hash_password(password,user['salt'])
-//     (user['password'] == hp) ? [user['auth'],user['apisecret']] : nil
-       return "ok"; 
+  checkUserCredentials: function(id, password) {
+    return this.lookupUserByID(id)
+      .then(function(user) {
+        if (true) { //user.password === hash_password(password, user.salt)) {
+          return [user.auth, user.apisecret]; 
+        }
+        else
+        {
+          return null;
+        }
       });
   },
 
-  lookupUserByUsername: function(username) {
+  lookupUserByID: function(id) {
+    var that = this;
+    return client.hgetallAsync("user:" + id)
+  },
+
+  userAsJSON: function(data) {
+    console.log(data);
+    return {
+      id:       data[0],
+      username: data[1],
+      salt:     data[2],
+      password: data[3],
+      ctime:    data[4],
+      karma:    data[5],
+      about:    data[6],
+      email:    data[7],
+      auth:     data[8],
+      apisecret: data[9],
+      flags:     data[10],
+      karma_incr_time: data[11]
+    }
+  },
+
+  lookupUserID: function(username) {
     return client.getAsync("username.to.id:" + username.toLowerCase());
   },
 }
@@ -54,4 +79,18 @@ function UserException(msg) {
      return this.value + this.message
   };
 }
+
+// Turn the password into an hashed one, using PBKDF2 with HMAC-SHA1
+// and 160 bit output.
+function hash_password(hash, salt) {
+  // p = PBKDF2.new do |p|
+  //     p.iterations = PBKDF2Iterations
+  //     p.password = password
+  //     p.salt = salt
+  //     p.key_length = 160/8
+  // end
+  // p.hex_string
+}
+
+
 UserException.prototype = Error.prototype;
